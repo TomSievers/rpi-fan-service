51362eb63273ee57861668f2f2f96aea290d4898
Tom Sievers <tom.sievers13@gmail.com>
2021-11-02 23:55:39 +0100

    Update dependencies needed for implementations of fan and temperature

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate toml;
+extern crate rppal;
+extern crate regex;
+extern crate serde;
+extern crate exitcode;
+extern crate syslog;
+#[macro_use]
+extern crate log;
+
+pub mod temp;
+pub mod fan;
 
 fn main() {
 }
